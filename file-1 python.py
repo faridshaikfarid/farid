e350570179861d5072daec0ed65d7991bdc285e1
faridshaikfarid <162982901+faridshaikfarid@users.noreply.github.com>
2024-03-11 16:06:44 +0530

    Add files via upload

---
+++ file-1 python.py
@@ -0,0 +1,85 @@
+
+# print ("hello world")
+
+#import this
+#print (this)
+
+#from file _1 import
+import antigravity
+
+
+
+Bargained character
+ Controls and Data structures-ABC
+ Strings and its formates-from C
+ Regular expressions-Pearl
+ Exception handling-Module 3
+ Magic methods from-fortron
+
+ Flovours Python
+ Cpythan-Written in C, Most common implementation of Python
+ Jythaon-Java implementation of Python
+ IronPython-Implementation of the Python Programming Language
+ which is tightly integrated with.NET.
+ Brython-Browser Python,runs in browsers
+ Rubypythin - Bridge between Python and Ruby Interpreters
+ Micropython-Runs in Micro Controllers
+
+ # --->print(object,sep, end, file, flush)
+ # print has 5 parameters
+
+ # object --->the value to be printed is object
+ #sep---> seperator
+ # Eg
+ # a = 8
+ # b = 9
+ # c = 89
+ # print (a,b,c sep='$')
+
+ # end ---> which is used to add the string at the end of the value,
+ # # It will not keep the command to stay in same line
+ # Eg
+ # print(str(a), b, end='%%')
+ # print(str(c))
+
+ #  ! ----> variables
+ # ?variable is a name which is used to identify the value of variables
+ # ?which is stored in a specific memory location
+
+ # ! Rules to define variable
+ # 1.) Variable have to start wth only alphabets, underscores
+ # 2.) The char following the 1st char can be alphabets, numbers, unders
+ # 3.) No spaces are allowed
+ # 4.) No special character like !@#23 $^&*()+
+ # 5.) case sensitive
+ # 6.) Keywords are not allowed as variables
+
+  #  ! ways to define variable
+
+  # a = 8
+  # b = 9
+  # prin(a,b)
+
+  # a = 7, b = 9 # error
+  # print(a,b)
+  # print  (a)
+  # print  (b)
+
+  #a=7
+  #b=7
+
+  #a = b = 7
+  #print(a,b)
+
+  # a=8, 7, 9.89, "hello"
+  # print(a)
+  # print(type(a))
+
+  # a, b = 78 #error
+  # print(a,b)
+  
+
+
+
+
+
